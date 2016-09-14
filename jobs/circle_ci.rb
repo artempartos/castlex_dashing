ec77da99b3eded38ad36bb3bd0e294a295f1905c
Artem Petrov <partos0511@gmail.com>
2016-09-14 21:10:50 +0400

    tyop fix

--- jobs/circle_ci.rb
+++ jobs/circle_ci.rb
@@ -39,32 +39,28 @@ def translate_status_to_class(status)
 end
 
 def build_data(project, auth_token)
-  begin
-    api_url = 'https://circleci.com/api/v1/project/%s/%s/tree/%s?circle-token=%s'
-    api_url = api_url % [project[:user], project[:repo], project[:branch], auth_token]
-    api_response =  HTTParty.get(api_url, :headers => { "Accept" => "application/json" } )
-    api_json = JSON.parse(api_response.body)
-    return {} if api_json.empty? || api_json['message'] == 'Project not found'
+  api_url = 'https://circleci.com/api/v1/project/%s/%s/tree/%s?circle-token=%s'
+  api_url = api_url % [project[:user], project[:repo], project[:branch], auth_token]
+  api_response =  HTTParty.get(api_url, :headers => { "Accept" => "application/json" } )
+  api_json = JSON.parse(api_response.body)
+  return {} if api_json.empty?
 
-    latest_build = api_json.select{ |build| build['status'] != 'queued' }.first
-    email_hash = Digest::MD5.hexdigest(latest_build['committer_email'])
-    build_id = "#{latest_build['branch']}, build ##{latest_build['build_num']}"
+  latest_build = api_json.select{ |build| build['status'] != 'queued' }.first
+  email_hash = Digest::MD5.hexdigest(latest_build['committer_email'])
+  build_id = "#{latest_build['branch']}, build ##{latest_build['build_num']}"
 
-    data = {
-      build_id: build_id,
-      repo: "#{project[:repo]}",
-      branch: "#{latest_build['branch']}",
-      time: "#{calculate_time(latest_build['stop_time'])}",
-      state: "#{latest_build['status'].capitalize}",
-      widget_class: "#{translate_status_to_class(latest_build['status'])}",
-      committer_name: latest_build['committer_name'],
-      commit_body: "\"#{latest_build['body']}\"",
-      avatar_url: "http://www.gravatar.com/avatar/#{email_hash}"
-    }
-    return data
-  rescue e
-    puts e
-  end
+  data = {
+    build_id: build_id,
+    repo: "#{project[:repo]}",
+    branch: "#{latest_build['branch']}",
+    time: "#{calculate_time(latest_build['stop_time'])}",
+    state: "#{latest_build['status'].capitalize}",
+    widget_class: "#{translate_status_to_class(latest_build['status'])}",
+    committer_name: latest_build['committer_name'],
+    commit_body: "\"#{latest_build['body']}\"",
+    avatar_url: "http://www.gravatar.com/avatar/#{email_hash}"
+  }
+  return data
 end
 
 SCHEDULER.every '10s', :first_in => 0  do
