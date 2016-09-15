b86f4bbe1fd62d0fd9fb73da699c749136e0805d
Artem Petrov <partos0511@gmail.com>
2016-09-15 04:47:48 +0400

    safe

--- jobs/codecov_prs.rb
+++ jobs/codecov_prs.rb
@@ -3,18 +3,23 @@ client = CodecovClient.new
 SCHEDULER.every '2m', first_in: 0 do |job|
   response = client.pull_requests('tundreus', 'learn_trials')
   remote_pulls = response['pulls']
-  return [] if remote_pulls.empty?
 
-  pulls = remote_pulls.select{|remote_pull| remote_pull['state'] == 'open'}.map do |remote_pull|
-    passed = remote_pull['head']['ci_passed']
-    title = "##{remote_pull['pullid']} #{remote_pull['title']} #{remote_pull['head']['totals']['c'].to_f.round(2)}"
+  pulls = []
+  pulls = remote_pulls.select{ |remote_pull| remote_pull['state'] == 'open'}
+  pulls = pulls.map do |remote_pull|
+    passed = remote_pull['head']['ci_passed'] rescue false
+    number = "##{remote_pull['pullid']}" rescue ""
+    coverage = remote_pull['head']['totals']['c'].to_f.round(2) rescue ''
+    title = "#{number} #{remote_pull['title']} #{coverage}"
     updated = DateTime.parse(remote_pull['updatestamp']).strftime("%b %-d %Y, %l:%m %p")
     color, arrow = get_label(passed)
+    message = remote_pull['head']['message'] rescue ""
+    user = "@#{remote_pull['head']['author']['username']}" rescue "@anon"
     {
       updated: updated,
       title: title,
-      message: remote_pull['head']['message'],
-      user: '@' + remote_pull['head']['author']['username'],
+      message: message,
+      user: user,
       color: color,
       arrow: arrow
     }
